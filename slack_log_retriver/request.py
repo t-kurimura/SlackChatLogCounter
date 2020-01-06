36ba5e405e271ae64637e7145121b224ec981f75
t-kurimura <takahisa.kurimura@eure.jp>
2020-01-06 19:34:07 +0900

    fix

--- slack_log_retriver/request.py
+++ slack_log_retriver/request.py
@@ -2,13 +2,13 @@ import csv
 import slack
 
 token = "{token}"
-latest_ts = 1578296339
-oldest_time = 1561906800
+oldest_ts = 1562405642
+latest_ts = 1578306643
 channel_id = "{channel_id}"
-search_word = "{search_keyword}"
+search_word = "{keyword}"
 
 
-def _get_filtered_ts_list(search_word, message_list):
+def _get_filtered_ts_list(search_keyword, message_list):
     ts_list = []
     for item in message_list:
         if item["text"] != "":
@@ -17,19 +17,24 @@ def _get_filtered_ts_list(search_word, message_list):
         if item["subtype"] != "bot_message":
             break
 
-        if search_word in item["attachments"][0]["fallback"]:
+        if item.get("icons") is None:
+            break
+
+        if item["icons"].get("emoji") != ":cry:":
+            break
+
+        if search_keyword not in item["attachments"][0]["fallback"]:
             break
 
         ts_list.append(int(float(item["ts"])))
-        print(int(float(item["ts"])))
 
     return ts_list
 
 
-def _out_to_csv(list):
+def _out_to_csv(input_list):
     with open("./output.csv", "x") as f:
         writer = csv.writer(f)
-        for item in list:
+        for item in input_list:
             writer.writerow([item])
 
 
@@ -41,27 +46,26 @@ def exec_all():
 
     current_latest_ts = latest_ts
 
-    while current_latest_ts > oldest_time:
+    while current_latest_ts > oldest_ts:
         response = None
         if cursor is None:
             response = client.conversations_history(
-                channel=channel_id, inclusive=1, oldest=oldest_time
+                channel=channel_id, inclusive=1, latest=latest_ts
             )
         else:
             response = client.conversations_history(
-                channel=channel_id, inclusive=1, oldest=oldest_time, cursor=cursor
+                channel=channel_id, inclusive=1, latest=latest_ts, cursor=cursor
             )
 
-        # retrieve last
-        for item in response["messages"]:
-            current_latest_ts = int(float(item["ts"]))
+        current_ts_list = _get_filtered_ts_list(search_word, response["messages"])
 
-        target_message_ts_list.append(
-            _get_filtered_ts_list(search_word, response["messages"])
-        )
+        if len(current_ts_list) > 0:
+            current_latest_ts = min(current_ts_list)
+
+        target_message_ts_list += current_ts_list
 
         if not response["has_more"]:
-            break
+            print(response)
         cursor = response["response_metadata"]["next_cursor"]
 
     _out_to_csv(target_message_ts_list)
