8a40931a1215464cfa7224d1fb5d6d8170dd5dc0
Takahisa Kurimura <kuri.226.t@gmail.com>
2020-03-29 04:12:07 +0900

    fix

--- slack_log_retriver/request.py
+++ slack_log_retriver/request.py
@@ -1,74 +1,94 @@
-import csv
+import re
+import urllib
+from datetime import date, datetime
+
 import slack
 
-token = "{token}"
-oldest_ts = 1562405642
-latest_ts = 1578306643
-channel_id = "{channel_id}"
-search_word = "{keyword}"
+token = ""
+
+user_id = ""
+start_dt = date(2020, 3, 1)
+end_dt = date(2020, 3, 29)
 
 
-def _get_filtered_ts_list(search_keyword, message_list):
-    ts_list = []
-    for item in message_list:
-        if item["text"] != "":
-            break
+def search_messages(query):
+    message_list = []
 
-        if item["subtype"] != "bot_message":
-            break
-
-        if item.get("icons") is None:
-            break
-
-        if item["icons"].get("emoji") != ":cry:":
-            break
-
-        if search_keyword not in item["attachments"][0]["fallback"]:
-            break
-
-        ts_list.append(int(float(item["ts"])))
-
-    return ts_list
-
-
-def _out_to_csv(input_list):
-    with open("./output.csv", "x") as f:
-        writer = csv.writer(f)
-        for item in input_list:
-            writer.writerow([item])
-
-
-def exec_all():
+    s_quote = urllib.parse.quote(query)
     client = slack.WebClient(token=token)
 
-    cursor = None
-    target_message_ts_list = []
+    for i in range(0, 100):
+        data = client.search_messages(query=s_quote, page=i, sort="timestamp", sort_dir="asc")
+        print(data.data['ok'], data.data["messages"]["paging"]["pages"], data.data["messages"]["paging"]["page"])
 
-    current_latest_ts = latest_ts
+        message_list.extend(data.data["messages"]["matches"])
+        if data.data["messages"]["paging"]["pages"] == 0:
+            print("could not find")
+            break
+        if data.data["messages"]["paging"]["pages"] == data.data["messages"]["paging"]["page"]:
+            break
 
-    while current_latest_ts > oldest_ts:
-        response = None
-        if cursor is None:
-            response = client.conversations_history(
-                channel=channel_id, inclusive=1, latest=latest_ts
-            )
-        else:
-            response = client.conversations_history(
-                channel=channel_id, inclusive=1, latest=latest_ts, cursor=cursor
-            )
-
-        current_ts_list = _get_filtered_ts_list(search_word, response["messages"])
-
-        if len(current_ts_list) > 0:
-            current_latest_ts = min(current_ts_list)
-
-        target_message_ts_list += current_ts_list
-
-        if not response["has_more"]:
-            print(response)
-        cursor = response["response_metadata"]["next_cursor"]
-
-    _out_to_csv(target_message_ts_list)
+    return message_list
 
 
-exec_all()
+def convert_to_dict(response_dict_list):
+    msg_array = []
+    msg_dict = {
+        'ts': [],
+        'parent_ts': [],
+        'is_parent': [],
+        'text': [],
+        'permalink': [],
+    }
+
+    for elm in response_dict_list:
+        m = re.search('[0-9]*\.[0-9]*$', elm["permalink"])
+        parent_ts = 0
+        if m is not None:
+            parent_ts = m.group(0)
+
+        msg_dict['ts'].append(datetime.fromtimestamp(float(elm["ts"])))
+        msg_dict['parent_ts'].append(datetime.fromtimestamp(float(parent_ts)))
+        msg_dict['is_parent'].append(elm["ts"] == parent_ts)
+        msg_dict['text'].append(elm["text"])
+        msg_dict['permalink'].append(elm["permalink"])
+
+        msg_array.append({
+            'ts': datetime.fromtimestamp(float(elm["ts"])),
+            'parent_ts': datetime.fromtimestamp(float(parent_ts)),
+            'is_parent': elm["ts"] == parent_ts,
+            'text': elm["text"],
+            'permalink': elm["permalink"],
+        })
+
+    result = []
+    for i in msg_array:
+        if i['parent_ts'] in msg_dict['ts']:
+            result.append(i)
+
+    return result
+
+
+def output(msg_array):
+    start_day = 0
+    for i in msg_array:
+        each_start_dt = i['ts'].day
+        if each_start_dt != start_day:
+            print(datetime.strftime(i['ts'], "%m/%d"))
+        start_day = each_start_dt
+        print(" {}, {}, {}".format(
+            datetime.strftime(i['ts'], "%H:%M"),
+            re.sub(r'<.*\||\n|>', '', i['text']),
+            i['permalink'],
+        ))
+
+
+query = "in:#eureka-attendance after:{} before:{} from:@{}".format(
+    start_dt.strftime("%Y-%m-%d"),
+    end_dt.strftime("%Y-%m-%d"),
+    user_id
+)
+print(query)
+msg_response_dict = search_messages(query)
+msg_arr = convert_to_dict(msg_response_dict)
+output(msg_arr)
